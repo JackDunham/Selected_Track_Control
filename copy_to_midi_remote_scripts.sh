1de1910e4a4c1bd93be3ca7565c27a6a83ba8e32
Jack Dunham <jack.dunham@gmail.com>
2016-06-10 10:25:43 -0700

    Version 1 of Logidy mappings

---
+++ copy_to_midi_remote_scripts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /Applications/Ableton\ Live\ 9.6.1\ Suite.app/Contents/App-Resources/MIDI\ Remote\ Scripts/STC
+cp *.py /Applications/Ableton\ Live\ 9.6.1\ Suite.app/Contents/App-Resources/MIDI\ Remote\ Scripts/STC/
